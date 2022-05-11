d150407af8c346440c7234b6fa4c1dc804ddc2aa
Ashini98K <ashini.kulatunga@gmail.com>
2022-05-11 20:35:41 +0530

    Implemented update and view inventory services

--- src/controllers/inventory_controller.js
+++ src/controllers/inventory_controller.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { route } = require("express/lib/application");
 const router = express.Router();
 const service = require("../services/inventory_service");
 
 module.exports = function () {
   router.post("/create", service.createInventory);
+  router.get("/view/supplier/:id", service.viewInventoryBySupplierId);
+  router.get("/view/:id", service.viewInventoryById);
+  router.put("/update/:id", service.updateInventory);
+
   return router;
 };
--- src/services/inventory_service.js
+++ src/services/inventory_service.js
@@ -14,6 +14,58 @@ const createInventory = async (req, res) => {
   }
 };
 
+const viewInventoryBySupplierId = async (req, res) => {
+  await Inventory.find({ supplierId: req.params.id })
+    .then((data) => {
+      res.status(200).send({ data: data });
+    })
+    .catch((error) => {
+      res.status(500).send({ error: error.message });
+    });
+};
+
+const viewInventoryById = async (req, res) => {
+  if (req.params && req.params.id) {
+    await Inventory.findById(req.params.id)
+      .then((data) => {
+        res.status(200).send({ data: data });
+      })
+      .catch((error) => {
+        res.status(500).send({ error: error.message });
+      });
+  }
+};
+
+const updateInventory = async (req, res) => {
+  if (!req.is("application/json")) {
+    res.send(400);
+  } else {
+    Inventory.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          itemName: req.body.itemName,
+          itemImage: req.body.itemImage,
+          itemDescription: req.body.itemDescription,
+          itemPrice: req.body.itemPrice,
+          itemQuantity: req.body.itemQuantity,
+        },
+      },
+      { upsert: true },
+      function (err, result) {
+        if (err) {
+          res.status(500).send(body);
+        } else {
+          res.status(200).send(result);
+        }
+      }
+    );
+  }
+};
+
 module.exports = {
   createInventory,
+  viewInventoryBySupplierId,
+  viewInventoryById,
+  updateInventory,
 };
